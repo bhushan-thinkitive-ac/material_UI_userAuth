bae82c0e5a23f33c7e2137838190e9e42538ba5e
Bhushan Chaudhari <bhushan.chaudhari@thinkitive.com>
2024-10-23 13:53:41 +0530

    Updated Login

--- src/Test.jsx
+++ src/Test.jsx
@@ -28,86 +28,151 @@ const Test = () => {
                 minHeight: '100vh',
                 justifyContent: 'center',
                 alignItems: 'center',
-                background: 'linear-gradient(90deg, #00c9ff 0%, #92fe9d 100%)',
+                background: 'linear-gradient(90deg, #0097B2 0%, #7ED957 100%)',
             }}
         >
-            <Grid2 item xs={12} sm={8} md={6}>
-                <Paper elevation={10} sx={{ display: 'flex', borderRadius: 5 }}>
-                    <Box
-                        sx={{
-                            flex: 180,
-                            backgroundColor: '#1f1f2e',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            borderRadius: '10px'
+            <Grid2
+                item
+                xs={12}
+                sm={8}
+                md={6}
+                container
+                direction={{ xs: 'column', md: 'row' }}
+                sx={{
+                    backgroundColor: 'white',
+                    borderRadius: 5,
+                    overflow: 'hidden',
+                    height: { xs: 'auto', md: '65vh' },
+                    width: { xs: '90vw', sm: '70vw', md: '55vw' },
+                    boxShadow: '0px 2px 50px rgba(0, 0, 0, 0.5)',
+                }}
+            >
+                <Box
+                    sx={{
+                        flex: 2,
+                        backgroundColor: '#1f1f2e',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: { xs: 2, md: 3 },
+                    }}
+                >
+                    <img
+                        src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2F672616f9a6e56be5377750fb39aa2788.cdn.bubble.io%2Ff1695325486924x576377181037805600%2FAdvanta%2520Final%2520%2528Black%2529.png?w=512&h=512&auto=compress&dpr=1.25&fit=max"
+                        alt="Logo"
+                        style={{
+                            width: '100%',
+                            maxWidth: { xs: '150px', sm: '200px', md: '500px' },
+                            objectFit: 'contain',
                         }}
-                    >
-                        <img
-                            src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2F672616f9a6e56be5377750fb39aa2788.cdn.bubble.io%2Ff1695325486924x576377181037805600%2FAdvanta%2520Final%2520%2528Black%2529.png?w=512&h=512&auto=compress&dpr=1.25&fit=max"
-                            alt="Logo"
-                            style={{ width: '80%', maxWidth: '1000px' }}
-                        />
-                    </Box>
-                    <Box
-                        sx={{
-                            flex: 200,
-                            padding: 4,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            borderRadius: '10px'
-                        }}
-                    >
-                        <Typography variant="h5" component="h1" gutterBottom>
-                            Sign In
-                        </Typography>
+                    />
+                </Box>
 
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            variant="outlined"
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            variant="outlined"
-                        />
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            sx={{
-                                mt: 3,
-                                mb: 2,
-                                background: 'linear-gradient(90deg, #43e97b 0%, #38f9d7 100%)',
-                                color: 'white',
-                                '&:hover': {
-                                    backgroundColor: '#ff000',
-                                },
-                            }}
-                            onClick={handleLogin}
-                        >
-                            Continue
-                        </Button>
-                        <Typography variant="body2" sx={{ textAlign: 'center' }}>
-                            Don't have an account? <Link to="/register">Register Here</Link>
-                        </Typography>
-                    </Box>
+                <Paper
+                    elevation={0}
+                    sx={{
+                        flex: 2,
+                        padding: { xs: 2, sm: 5, md: 8 },
+                        marginTop: { xs: 2, md: 6 },
+                    }}
+                >
+                    <Typography
+                        variant="h4"
+                        component="h1"
+                        gutterBottom
+                        sx={{
+                            fontFamily: '"Poppins", sans-serif',
+                            fontSize: { xs: '24px', sm: '32px', md: '36px' },
+                        }}
+                    >
+                        Sign In
+                    </Typography>
+
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        variant="standard"
+                        InputProps={{
+                            style: {
+                                borderBottom: '3px solid #43e97b',
+                                fontSize: { xs: '14px', sm: '16px' },
+                                fontFamily: '"Poppins", sans-serif',
+                            },
+                            disableUnderline: true,
+                        }}
+                        InputLabelProps={{
+                            style: {
+                                fontSize: { xs: '12px', sm: '14px' },
+                                fontFamily: '"Poppins", sans-serif',
+                            },
+                        }}
+                    />
+
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        variant="standard"
+                        InputProps={{
+                            style: {
+                                borderBottom: '2px solid #43e97b',
+                                fontSize: { xs: '14px', sm: '16px' },
+                                fontFamily: '"Poppins", sans-serif',
+                            },
+                        }}
+                        InputLabelProps={{
+                            style: {
+                                fontSize: { xs: '12px', sm: '14px' },
+                                fontFamily: '"Poppins", sans-serif',
+                            },
+                        }}
+                    />
+
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        sx={{
+                            mt: 3,
+                            mb: 2,
+                            backgroundColor: '#f0f0f0',
+                            color: '#ffffff',
+                            padding: '15px 0',
+                            fontSize: { xs: '16px', md: '18px' },
+                            height: '60px',
+                            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
+                            borderRadius: '5px',
+                            '&:hover': {
+                                background: 'linear-gradient(90deg, #0097B2 0%, #7ED957 100%)',
+                            },
+                        }}
+                        onClick={handleLogin}
+                    >
+                        Continue
+                    </Button>
+
+                    <Typography
+                        variant="body2"
+                        sx={{
+                            textAlign: 'center',
+                            fontSize: { xs: '12px', sm: '14px' },
+                        }}
+                    >
+                        Don't have an account? <Link to="/register">Register Here</Link>
+                    </Typography>
                 </Paper>
             </Grid2>
         </Grid2>
